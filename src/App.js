8dde367c45e922cd1dbec312911feb9fbd71632f
manthila1997 <manthilab@gmail.com>
2021-05-27 12:52:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className=" text-3xl font-bold italic text-red-500">
       This is the first Page.
     </div>
   );
