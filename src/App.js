e9847aa066e2bea4fc53b87b54c8f206cdc965cf
manthila1997 <manthilab@gmail.com>
2021-05-27 13:32:52 +0530

    initial check

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className=" text-3xl font-bold italic text-red-500">
-      This is the first Page.
+      This is the first Page. 2
     </div>
   );
 }
