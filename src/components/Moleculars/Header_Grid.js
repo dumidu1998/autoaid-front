2d763daa77b0b3dfdc5d84e78c79da6db9108142
manthila1997 <49613631+manthila1997@users.noreply.github.com>
2021-06-01 11:41:11 +0530

    new imgs

--- src/components/Moleculars/Header_Grid.js
+++ src/components/Moleculars/Header_Grid.js
@@ -5,10 +5,10 @@ export default function Header_Grid() {
     return (
         <div>
             <div className="flex flex-col md:flex-row w-10/12 mx-auto items-center justify-center">
-                <Card_services service="Service "/>
-                <Card_services service="General Repairs"/>
-                <Card_services service=" Wheel Allignment"/>
-                <Card_services service="High tech"/>
+                <Card_services service="Service " img="/imgs/icons/Servicewash.png"/>
+                <Card_services service="General Repairs" img="/imgs/icons/services.png"/>
+                <Card_services service=" Wheel Allignment" img="/imgs/icons/Wheelalignment.png"/>
+                <Card_services service="High tech" img="/imgs/icons/HighTech.png"/>
             </div>
         </div>
     )
