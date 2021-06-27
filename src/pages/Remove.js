fd9e1b0b3d8fb9ccedea0238997a0bf2c5bb60d5
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-06-27 13:23:30 +0530

    test file added

---
+++ src/pages/Remove.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+class fjdj extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <>
+            </>
+         );
+    }
+}
+ 
+export default fjdj;
