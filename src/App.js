b96decaad92e82af052c82598f64b2d4f90d6003
dumidu1998 <dumiduraj@gmail.com>
2021-07-19 12:18:54 +0530

    links added, props added, technician dashboard done

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import VehicleDetails from './pages/customer/VehicleProfile'
 
 import AdminDashboard from './pages/admin/AdminDashboard';
 import AdminStaff from './pages/admin/AdminStaff';
+import Dashboard from './pages/technician/Dashboard';
 
 
 
@@ -20,18 +21,19 @@ function App() {
   return (
     <div className="overflow-hidden">
 
-    {/* <NavBar/> */}
+      {/* <NavBar/> */}
       <Router>
-          <Switch>
-            <Route path="/" exact component={Landing} />
-            <Route path="/signup" exact component={Signup} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/serviceadvisor" exact component={Service} />
-            <Route path="/test" exact component={Test} />
-            <Route path="/customer" exact component={Customer} />
-            <Route path="/customer/vehicle" exact component={VehicleDetails} />
-            <Route path="/admin" exact component={AdminDashboard} />
-            <Route path="/adminstaff" exact component={AdminStaff} />
+        <Switch>
+          <Route path="/" exact component={Landing} />
+          <Route path="/signup" exact component={Signup} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/serviceadvisor" exact component={Service} />
+          <Route path="/test" exact component={Test} />
+          <Route path="/customer" exact component={Customer} />
+          <Route path="/customer/vehicle" exact component={VehicleDetails} />
+          <Route path="/admin" exact component={AdminDashboard} />
+          <Route path="/adminstaff" exact component={AdminStaff} />
+          <Route path="/technician/dashboard" exact component={Dashboard} />
         </Switch>
       </Router>
 
