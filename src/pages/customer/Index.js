3b159fd8d3e7bc60eca828e41df82d469ae13680
manthila1997 <49613631+manthila1997@users.noreply.github.com>
2021-07-20 20:10:04 +0530

    be

--- src/pages/customer/Index.js
+++ src/pages/customer/Index.js
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BottomNav from '../../components/Moleculars/customer/BottomNav'
-import Addnew from '../../components/Atoms/serviceAdvisor/Addnew'
+import Addnew from '../../components/Atoms/serviceStation/Addnew'
 import HeadingPRimary from '../../components/Atoms/customer/HeadingPRimary'
 import DetailsShowing from '../../components/Moleculars/customer/DetailsShowing'
 import SideLink from '../../components/Atoms/customer/SideLink'
 import VehicleContainer from '../../components/Moleculars/customer/VehicleContainer'
 import StatusViewer from '../../components/Moleculars/customer/StatusViewer'
+import axios from 'axios';
 import TopNav from '../../components/Moleculars/customer/TopNav'
 export default function Index() {
+ const [vehicles, setvehicles] = useState([])
 
     var submit = () => {
-        axios.get(`${process.env.REACT_APP_API_BASE_URL}/customer/1`)
+        
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/customer/2`)
             .then(function (response) {
                 // handle success
-                console.log(response);
-                alert(response.data);
+                setvehicles(response.data);
+                console.log(vehicles);
+                // alert(response.data);
 
             })
             .catch(function (error) {
                 // handle error
-                toast.error('❌ ' + error.response.data);
-                // alert(error.response.data);
+                console.log(error.response.data);
             })
             .then(function () {
                 // always executed
@@ -31,7 +34,9 @@ export default function Index() {
     useEffect(() => {
         submit();
     }, [])
-    
+
+   
+
     return (
         <div className="mb-44">
         <div className="flex flex-col items-center">
@@ -63,8 +68,14 @@ export default function Index() {
                 </div>
                     <div className="flex flex-col justify-center md:bg-white md:p-5 md:rounded-lg md:shadow-lg md:w-1/3 md:my-5 md:mx-5 ">
                         <HeadingPRimary heading="Vehicles" />
-                        <VehicleContainer reg="CAM-9044" time="200km / 3 Months"/>
-                        <VehicleContainer reg="CAM-9044" time="200km / 3 Months"/>
+                        {vehicles.map((vehicle, index) => {
+                // console.log(course.title)
+                        return (
+                            
+                            <VehicleContainer reg={vehicle.vehicleNumber} time="200km / 3 Months"/>
+                        );
+                    })}
+                        
                     </div>
                 </div>
 
