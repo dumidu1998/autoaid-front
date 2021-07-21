e9ac6a425b6fe28567b7e2ee901d34bdde19c9a0
dumidu1998 <dumiduraj@gmail.com>
2021-07-21 14:19:41 +0530

    Update get data done

--- src/components/Organs/admin/AdminStaffManageOrgan.js
+++ src/components/Organs/admin/AdminStaffManageOrgan.js
@@ -1,20 +1,20 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import StaffManageFormOrgan from './StaffManageFormOrgan';
 import StaffMemListOrgan from './StaffMemListOrgan';
 import StaffNavbarMolecular from '../../Moleculars/admin/StaffNavbarMolecular';
 
-export default function AdminStaffManageOrgan() {
+export default function AdminStaffManageOrgan(props) {
     const [added, setadded] = useState(true);
     return (
         <div>
-                <div className="mb-3">
-                    <StaffNavbarMolecular/>
-                </div>
-                <div className="flex items-center justify-center">
-                    <StaffMemListOrgan added={added} />
-                    <div className="ml-12"></div> 
-                    <StaffManageFormOrgan setadded={setadded} added={added} />
-                </div>
+            <div className="mb-3">
+                <StaffNavbarMolecular />
             </div>
+            <div className="flex items-center justify-center">
+                <StaffMemListOrgan added={added} />
+                <div className="ml-12"></div>
+                <StaffManageFormOrgan setadded={setadded} added={added} staffdetails={props.staffdetails} />
+            </div>
+        </div>
     )
 }
