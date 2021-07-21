b8bba101c2a9a5cf41d6da967a4f0e003f96d176
dumidu1998 <dumiduraj@gmail.com>
2021-07-21 21:41:12 +0530

    new drop down added

--- src/components/Moleculars/technician/TechniciansDropDown.js
+++ src/components/Moleculars/technician/TechniciansDropDown.js
@@ -1,14 +1,115 @@
-import React from 'react'
+/* This example requires Tailwind CSS v2.0+ */
+import { Fragment, useState } from 'react'
+import { Listbox, Transition } from '@headlessui/react'
+// import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
+
+const people = [
+    {
+        id: 1,
+        name: 'Hasantha Lakshan',
+        avatar:
+            'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+        id: 2,
+        name: 'Amod Lakshan',
+        avatar:
+            'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+    {
+        id: 3,
+        name: 'Dumidu Kasun',
+        avatar:
+            'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
+    },
+    {
+        id: 4,
+        name: 'Madhawa Dissanayake',
+        avatar:
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    },
+]
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
 
 export default function TechniciansDropDown() {
+    const [selected, setSelected] = useState(people[3])
+
     return (
-        <div>
-            <select class="form-select mt-1 block w-full rounded-lg">
-                <option>$1,000</option>
-                <option>$5,000</option>
-                <option>$10,000</option>
-                <option>$25,000</option>
-            </select>
-        </div>
+        <Listbox value={selected} onChange={setSelected}>
+            {({ open }) => (
+                <>
+                    <div className="mt-1 relative w-3/4">
+                        <Listbox.Button className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <span className="flex items-center">
+                                <img src={selected.avatar} alt="" className="flex-shrink-0 h-6 w-6 rounded-full" />
+                                <span className="ml-3 block truncate">{selected.name}</span>
+                            </span>
+                            <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                                {/* <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" /> */}
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
+                                </svg>
+                            </span>
+                        </Listbox.Button>
+
+                        <Transition
+                            show={open}
+                            as={Fragment}
+                            leave="transition ease-in duration-100"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Listbox.Options
+                                static
+                                className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+                            >
+                                {people.map((person) => (
+                                    <Listbox.Option
+                                        key={person.id}
+                                        className={({ active }) =>
+                                            classNames(
+                                                active ? 'text-white bg-indigo-600' : 'text-gray-900',
+                                                'cursor-default select-none relative py-2 pl-3 pr-9'
+                                            )
+                                        }
+                                        value={person}
+                                    >
+                                        {({ selected, active }) => (
+                                            <>
+                                                <div className="flex items-center">
+                                                    <img src={person.avatar} alt="" className="flex-shrink-0 h-6 w-6 rounded-full" />
+                                                    <span
+                                                        className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
+                                                    >
+                                                        {person.name}
+                                                    </span>
+                                                </div>
+
+                                                {selected ? (
+                                                    <span
+                                                        className={classNames(
+                                                            active ? 'text-white' : 'text-indigo-600',
+                                                            'absolute inset-y-0 right-0 flex items-center pr-4'
+                                                        )}
+                                                    >
+                                                        {/* <CheckIcon className="h-5 w-5" aria-hidden="true" /> */}
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />
+                                                        </svg>
+                                                    </span>
+                                                ) : null}
+                                            </>
+                                        )}
+                                    </Listbox.Option>
+                                ))}
+                            </Listbox.Options>
+                        </Transition>
+                    </div>
+                </>
+            )}
+        </Listbox>
     )
 }
