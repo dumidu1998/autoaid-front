c2953aa85adc0959fa5fd34563e05add90f96453
dumidu1998 <dumiduraj@gmail.com>
2021-07-21 16:39:28 +0530

    amod edit

--- src/components/Atoms/admin/StaffNavBar.js
+++ src/components/Atoms/admin/StaffNavBar.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import StaffManageNavbarHeadingBtns from './StaffManageNavbarHeadingBtns';
 class StaffNavBar extends Component {
-    state = { 
-        list:[  {id:1,value:"Admin"},
-                {id:2,value:"Service Advisor"},
-                {id:3,value:"Technician"},
-                {id:4,value:"Cashier"},
-                {id:5,value:"Stock Keeper"},
-            ]
-     }
+    state = {
+        list: [{ id: 1, value: "Admin" },
+        { id: 2, value: "Service Advisor" },
+        { id: 3, value: "Technician" },
+        { id: 4, value: "Cashier" },
+        { id: 5, value: "Stock Keeper" },
+        ]
+    }
 
-    render() { 
-        return ( 
+    render() {
+        return (
             <di className="flex">
-               {this.state.list.map(btn=><StaffManageNavbarHeadingBtns key={btn.id} btnName={btn.value}  id={btn.id}/>)}
+                {this.state.list.map(btn => <StaffManageNavbarHeadingBtns key={btn.id} btnName={btn.value} id={btn.id} />)}
             </di>
-         );
+        );
     }
 }
- 
+
 export default StaffNavBar;
