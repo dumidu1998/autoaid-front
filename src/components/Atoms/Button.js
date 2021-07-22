2865856f4bc3054805cb984f117f5c00ef9e8a18
Hasantha96 <nphlakshan@gmail.com>
2021-07-22 09:39:05 +0530

    register link

--- src/components/Atoms/Button.js
+++ src/components/Atoms/Button.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 export default function Button(props) {
     return (
         <div>
-            <div className="w-24 h-10 bg-primary-0 rounded-md flex items-center justify-center hover:shadow-lg">
-                <h1 className=" font-primary">{props.txt}</h1>
-            </div>
+            <Link to={props.link}>
+                <div className="w-24 h-10 bg-primary-0 rounded-md flex items-center justify-center hover:shadow-lg">
+                    <h1 className=" font-primary">{props.txt}</h1>
+                </div>
+            </Link>
         </div>
     )
 }
