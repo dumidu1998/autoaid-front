f40bb7595a6cc37adfee1dee203198b33c376205
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-08-11 13:47:34 +0530

    section manage page added

---
+++ src/pages/admin/AdminSection.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AdminSideBar from '../../components/Moleculars/admin/AdminSideBar'
+
+export default function AdminSection() {
+    return (
+        <div className="relative bg-Background-0">
+            <AdminSideBar name="DashBoard " roleName="Admin"/>
+        </div>
+    )
+}
