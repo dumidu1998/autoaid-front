26d19c6cdd4c92897b356ee9e4f2fcce8e9a7aeb
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-08-12 07:24:44 +0530

    serviceAdvisor Dashboard

--- src/components/Atoms/serviceStation/TopContainer.js
+++ src/components/Atoms/serviceStation/TopContainer.js
@@ -5,11 +5,11 @@ import ProfileIcon from './ProfileIcon'
 export default function TopContainer(props) {
     return (
         <div>
-            <div className="w-full h-24 bg-forth-0 rounded-b-3xl flex items-center justify-center">
-                <div className=" flex w-9/12 justify-between items-center">
+            <div className="w-full h-24 bg-forth-0 rounded-b-3xl flex ">
+                <div className=" flex  w-full justify-between items-center">
 
-                    <h1 className=" font-primary text-3xl text-white">{props.heading1} <span className=" font-primary text-2xl text-white font-light">({props.heading2})</span></h1>
-                    <div className="flex items-center justify-center">
+                    <h1 className=" font-primary text-3xl text-white ml-16">{props.heading1} <span className=" font-primary text-2xl text-white font-light">({props.heading2})</span></h1>
+                    <div className="flex ">
                         <Addnew txt={props.addnewbtntext} />
                         <ProfileIcon />
                     </div>
--- src/pages/serviceAdvisor/Dashboard.js
+++ src/pages/serviceAdvisor/Dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AppointmentContainer from '../../components/Atoms/serviceStation/AppointmentContainer'
+import OngoingServiceDetails from '../../components/Atoms/serviceStation/OngoingServiceDetails'
 import Register from '../../components/Atoms/serviceStation/Register'
 import SubSectionHeading from '../../components/Atoms/serviceStation/SubSectionHeading'
 import TopContainer from '../../components/Atoms/serviceStation/TopContainer'
@@ -10,21 +11,55 @@ export default function Dashboard() {
         <div className=" bg-Background-0">
             <div className="flex flex-row">
                 <div className="">
-
                     <SideNav />
                 </div>
                 <div className="w-full flex flex-col">
-
-                    <TopContainer />
-                    <div className="h-full flex items-center justify-center">
-                        <div className="h-full w-10/12 py-24">
-                            <SubSectionHeading heading="Next Appointment" />
-                            <AppointmentContainer />
+                    <TopContainer heading1="Dashboard" heading2="Service Advisor" addnewbtntext="Add New"/>
+                    <div className="flex justify-between">
+                        <div className="w-full flex flex-col items-center justify-center">
+                            <div className=" w-10/12 pt-24">
+                                <SubSectionHeading heading="Next Appointment" />
+                                <AppointmentContainer vehicleNo="CAM - 9044" link="" />
+                            </div>
+                            <div className="pt-16 w-10/12 h-96">
+                                <SubSectionHeading heading="Upcoming Appointment" />
+                                <div className="bg-Background-0  h-full overflow-auto ">
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                </div>
+                            </div>
                         </div>
-
+                        <div className=" w-10/12 pt-24 mr-14">
+                            <SubSectionHeading heading="Ongoing Service Details" />  
+                            <div className="bg-white w-full h-5/6 rounded-xl shadow-xl  py-12">
+                                <div className="px-12">
+                                    <OngoingServiceDetails RegNum="Reg. No" CurrentStatus="Current Status"/>
+                                </div>
+                                <div className="w-full h-64 overflow-auto">
+                                   <div className="mt-4 border-b-2 pr-16 pl-12"><OngoingServiceDetails RegNum="CAM - 4090" CurrentStatus="Completed"/></div>
+                                   <div className="mt-4 border-b-2 pr-16 pl-12"><OngoingServiceDetails RegNum="CAM - 4090" CurrentStatus="On service"/></div>
+                                   {/* <div className="mt-4 border-b-2 pr-9 pl-12"><OngoingServiceDetails RegNum="CAM - 4090" CurrentStatus="Completed"/></div>
+                                   <div className="mt-4 border-b-2 pr-9 pl-12"><OngoingServiceDetails RegNum="CAM - 4090" CurrentStatus="On service"/></div>
+                                   <div className="mt-4 border-b-2 pr-9 pl-12"><OngoingServiceDetails RegNum="CAM - 4090" CurrentStatus="Completed"/></div>
+                                   <div className="mt-4 border-b-2 pr-9 pl-12"><OngoingServiceDetails RegNum="CAM - 4090" CurrentStatus="On service"/></div> */}
+                              
+                                  
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <Register />
+                
 
             </div>
         </div>
