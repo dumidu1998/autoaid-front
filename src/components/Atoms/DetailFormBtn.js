12317f6ed5932a7e32fb553db02d1f0284c7a977
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-08-13 22:08:17 +0530

    form service ad

---
+++ src/components/Atoms/DetailFormBtn.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+
+export default function DetailFormBtn(props) {
+    return (
+             <div>
+                <Link to={props.link}>
+                    <div className="w-24 h-10 bg-primary-0 rounded-md flex items-center justify-center hover:shadow-lg">
+                        <h1 className=" font-primary text-white font-medium">{props.txt}</h1>
+                    </div>
+                </Link>
+            </div>
+    )
+}
