75f60805fab3bf038edc93c4dbc4a0488c967fba
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-08-17 12:01:11 +0530

    navbar

---
+++ src/components/Atoms/serviceStation/SelectionSectionNavbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function SelectionSectionNavbar(props) {
+    return (
+        <div className="w-full flex justify-center items-center">
+            <button className="bg-black w-24 h-16 focus:backgroundColor:bg-white">Service</button>
+            
+
+            
+        </div>
+    )
+}
