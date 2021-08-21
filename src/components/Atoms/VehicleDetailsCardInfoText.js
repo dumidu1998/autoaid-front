b60d438dbb32e0de0707f14a705734914d34277e
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-08-21 21:30:57 +0530

    SectionManageInfocards

--- src/components/Atoms/VehicleDetailsCardInfoText.js
+++ src/components/Atoms/VehicleDetailsCardInfoText.js
@@ -4,7 +4,7 @@ export default function VehicleDetailsCardInfoText(props) {
     return (
         <div className="flex flex-col font-primary mt-4">
         <div>{props.heading}</div>
-        <div className="font-bold">{props.info}</div>
+        <div className="font-bold mt-2">{props.info}</div>
     </div>
     )
 }
