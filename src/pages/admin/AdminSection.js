50bf44e2a657f60958eb9931e2dfa20c7027bb1d
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-08-21 23:20:25 +0530

    SectionManageV3

--- src/pages/admin/AdminSection.js
+++ src/pages/admin/AdminSection.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import GoBtn from '../../components/Atoms/GoBtn'
 import SectionCardsHeadings from '../../components/Atoms/serviceStation/SectionCardsHeadings'
 import SectionsCardsTechnisianInchage from '../../components/Atoms/serviceStation/SectionsCardsTechnisianInchage'
 import AdminSideBar from '../../components/Moleculars/admin/AdminSideBar'
@@ -14,7 +15,7 @@ export default function AdminSection() {
                         <div className="text-xl font-primary font-medium flex justify-center items-center">General Repair </div>
                         <div className="flex justify-between items-center">
                             <div className="flex flex-col">
-                                <SectionCardsHeadings heading="Total No of Slots" count="5" class="font-medium "/>
+                                <SectionCardsHeadings heading="Total No of Slots" count="5" class="font-medium ml-12    "/>
                                 <SectionCardsHeadings heading="Free Slots" count="5" class="font-bold text-green-500"/>
                                 <SectionCardsHeadings heading="Occupied Slots" count="5" class="font-bold text-red-600"/>
                             </div>
@@ -22,9 +23,21 @@ export default function AdminSection() {
                                 <SectionsCardsTechnisianInchage technician="Amod Pathi"/>
                             </div>
                         </div>
+                        <div className="mt-8 flex justify-center items-center"><GoBtn btnName="Go"/></div>
                     </div>
-                    <div className="bg-white w-5/12  rounded-lg shadow-2xl">
-    kk
+                    <div className="bg-white w-5/12 rounded-lg shadow-2xl p-6">
+                        <div className="text-xl font-primary font-medium flex justify-center items-center">Engine Repair </div>
+                        <div className="flex justify-between items-center">
+                            <div className="flex flex-col">
+                                <SectionCardsHeadings heading="Total No of Slots" count="5" class="font-medium ml-12    "/>
+                                <SectionCardsHeadings heading="Free Slots" count="5" class="font-bold text-green-500"/>
+                                <SectionCardsHeadings heading="Occupied Slots" count="5" class="font-bold text-red-600"/>
+                            </div>
+                            <div>
+                                <SectionsCardsTechnisianInchage technician="Amod Pathi"/>
+                            </div>
+                        </div>
+                        <div className="mt-8 flex justify-center items-center"><GoBtn btnName="Go"/></div>
                     </div>
                 </div>
             </div>
