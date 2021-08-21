6c69380ad94f1ce0f8b5821baa63b7963875f6c3
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-08-21 15:31:29 +0530

    ServiceHistroyV3.0

--- src/pages/serviceAdvisor/ServiceHistory.js
+++ src/pages/serviceAdvisor/ServiceHistory.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import ServiceHistoryCards from '../../components/Atoms/ServiceHistoryCards'
 import TopContainerVNo from '../../components/Atoms/technician/TopContainerVNo' 
+import VehicleDetailsCardInfo from '../../components/Atoms/VehicleDetailsCardInfo'
+import VehicleDetailsCardInfoText from '../../components/Atoms/VehicleDetailsCardInfoText'
 import ServiceHistoryNavBar from '../../components/Moleculars/serviceAdvisor/ServiceHistoryNavBar'
 import SideNav from '../../components/Moleculars/serviceAdvisor/sideNav'
 
@@ -26,16 +28,18 @@ export default function ServiceHistory() {
                            <ServiceHistoryCards name="Noice in the Engine" price="9500"/>
                         </div>
                     </div>
-                    <div className="w-2/6 bg-white shadow-xl rounded-lg mt-12  mr-32 p-8">
-                        heding
-                        <div className="mt-6 mb-4">
-                           jj
+                    <div className="w-2/6 bg-white shadow-xl rounded-lg mt-12  mr-32 p-8 px-16">
+                        <div className="font-primary text-2xl font-medium flex justify-center items-center m-12">Vehicle Details</div>
+                        <div className="mt-6 mb-4 flex justify-between items-center">
+                           <VehicleDetailsCardInfo color="text-red-600 mt-2 font-bold" heading="VIN No." info="1234"/>
+                           <VehicleDetailsCardInfo color="text-black mt-2 font-bold" heading="Chassis No." info="345FG"/>
+                        </div>
+                        <div className="mt-6 mb-4 flex justify-between items-center">
+                           <VehicleDetailsCardInfo color="text-black mt-2 font-bold" heading="Engine No." info="3456"/>
+                           <VehicleDetailsCardInfoText heading="Vehicle Model" info="X-trail"/>
                         </div>
                         <div className="mt-6">
-                           kk
-                        </div>
-                        <div className="mt-6">
-                            k   
+                           <VehicleDetailsCardInfoText heading="Vehicle Type" info="CAR(hy-brid)"/>
                         </div>
                     </div>
                 </div>
