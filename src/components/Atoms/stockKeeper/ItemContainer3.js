09dedbf431398e726b566a70676e1c4c33856acb
Madhawa Dissanayake <73326224+MadhawaDissanayake@users.noreply.github.com>
2021-08-24 19:01:17 +0530

    Update ItemContainer3.js

--- src/components/Atoms/stockKeeper/ItemContainer3.js
+++ src/components/Atoms/stockKeeper/ItemContainer3.js
@@ -6,8 +6,8 @@ export default function ItemContainer3(props) {
         <div>
             <div className=" w-full h-10 shadow-lg bg-white mt-5 rounded-lg flex items-center justify-center">
                 <div className="flex flex-row w-10/12 items-center justify-between">
-                    <h1 className="font-bold font-primary text-gray-400">{props.itemNo}</h1>
-                    <h1 className="font-normal font-primary ">{props.quantity}</h1>
+                    <p className="font-bold font-primary text-gray-400 justify">{props.itemNo}</p>
+                    <p className="font-normal font-primary ">{props.quantity}</p>
                     <ButtonSecond link={props.link} />
 
                 </div>
