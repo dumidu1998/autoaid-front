26418178c2063e82337497c982d6e2940fb879d8
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-08-27 19:14:04 +0530

    TechnicianAsignedV2

--- src/pages/admin/AdminAsignTechnician.js
+++ src/pages/admin/AdminAsignTechnician.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useParams,useLocation } from 'react-router-dom/cjs/react-router-dom.min'
 import AddSlotBtn from '../../components/Atoms/admin/AddSlotBtn';
+import SlotNumberCard from '../../components/Atoms/admin/SlotNumberCard';
 import AdminSideBar from '../../components/Moleculars/admin/AdminSideBar'
+import TechniciansDropDown from '../../components/Moleculars/technician/TechniciansDropDown';
 
 export default function AdminAsignTechnician(props) {
     const location = useLocation()
@@ -15,6 +17,19 @@ export default function AdminAsignTechnician(props) {
                 <div className="px-3 md:px-8 -mt-24">
                     <div className="container mx-auto max-w-full">
                         <div className="flex justify-center items-center mt-32 md:mt-16 md:justify-end md:mr-10"><AddSlotBtn/></div>
+                        <div className="bg-white shadow-xl rounded-lg w-full px-8 py-12 mt-6">
+                            <div className="flex justify-center items-center">
+                                <TechniciansDropDown/>  
+                            </div>
+                            <div className="flex flex-col justify-center items-center mt-6 sm:grid grid-cols-2 place-items-center lg:grid-cols-3 xl:grid-cols-4">
+                                <SlotNumberCard/>
+                                <SlotNumberCard/>
+                                <SlotNumberCard/>
+                                <SlotNumberCard/>
+                                <SlotNumberCard/>
+                            </div>
+                            
+                        </div>
                     </div>
                 </div>
             </div>
