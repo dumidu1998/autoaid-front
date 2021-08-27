ca666e957502eebb5628992903d69682861b582c
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-08-27 12:05:34 +0530

    StaffManageResponsiveV4

--- src/components/Moleculars/admin/StaffNavbarMolecular.js
+++ src/components/Moleculars/admin/StaffNavbarMolecular.js
@@ -4,7 +4,7 @@ import StaffManageBtn from '../../Atoms/admin/StaffManageBtn';
 import StaffNavBar from '../../Atoms/admin/StaffNavBar';
 export default function StaffNavbarMolecular(props) {
         return (
-            <div className="flex flex-col mt-4 justify-between items-center">
+            <div className="flex flex-col mt-4 lg:mt-2 justify-between items-center">
                 <div className=" bg-white h-auto flex justify-center items-center w-11/12 rounded-xl shadow-2xl">
                     <StaffNavBar setfilter={props.setfilter} filter={props.filter} setUserType={props.setUserType} userType={props.userType} />
                 </div>
