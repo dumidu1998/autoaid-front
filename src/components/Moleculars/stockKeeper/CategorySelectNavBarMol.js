e88d93b30772ba8991ab23bd9f285b815bf94811
Madhawa Dissanayake <73326224+MadhawaDissanayake@users.noreply.github.com>
2021-08-28 10:01:38 +0530

    item name search in stock page created

--- src/components/Moleculars/stockKeeper/CategorySelectNavBarMol.js
+++ src/components/Moleculars/stockKeeper/CategorySelectNavBarMol.js
@@ -4,11 +4,30 @@ import SectionSelectNavbar from '../../Atoms/stockKeeper/SectionSelectNavbar';
 export default function CategorySelectNavBarMol() {
 
     const [list, setList] = useState([
-        { id: 1, value: "Engine" },
+        { id: 0, value: "All" },
+        { id: 1, value: "A/C" },
         { id: 2, value: "Brakes" },
         { id: 3, value: "Clutch" },
         { id: 4, value: "Suspension" },
         { id: 5, value: "Body" },
+        { id: 6, value: "Body" },
+        { id: 7, value: "Body" },
+        { id: 8, value: "Body" },
+        { id: 9, value: "Body" },
+        { id: 10, value: "Body" },
+        { id: 11, value: "All" },
+        { id: 12, value: "A/C" },
+        { id: 13, value: "Brakes" },
+        { id: 14, value: "Clutch" },
+        { id: 15, value: "Suspension" },
+        { id: 16, value: "Body" },
+        { id: 17, value: "Body" },
+        { id: 18, value: "Body" },
+        { id: 19, value: "Body" },
+        { id: 20, value: "Body" },
+        { id: 21, value: "Body" },
+
+
       
     ])
 
@@ -18,7 +37,7 @@ export default function CategorySelectNavBarMol() {
     
     return (
         <div className=" bg-white p-2 shadow-2xl rounded-lg grid grid-cols-1 mb-2">
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between overflow-auto">
                 {list.map(btn => <SectionSelectNavbar setHighBtn={setHighBtn} highBtn={highBtn} key={btn.id} name={btn.value} id={btn.id} /> )}
             </div>
         </div>
--- src/pages/StockKeeper/StockQuantity.js
+++ src/pages/StockKeeper/StockQuantity.js
@@ -13,11 +13,15 @@ import ButtonRedProps from '../../components/Atoms/stockKeeper/ButtonRedProps'
 export default function StockQuantity() {
     const [result, setresult] = useState([]);
     const [show, setshow] = useState("hidden");
+    const [output, setoutput] = useState({itemName:"",itemNo:"",stock:"",price:"",reorderLevel:""});
 
     function showdetails(id) {
         axios.get(`${process.env.REACT_APP_API_BASE_URL}/inventory/itembyid/${id.target.id}`)
             .then(res => {
                 setshow("hidden");
+                // window.document.getElementById("name")=res.data.itemName;
+               setoutput(res.data);
+               console.log(output);
             }
             ).catch(err => {
                 console.log(err);
@@ -33,6 +37,7 @@ export default function StockQuantity() {
             .then(res => {
                 setresult(res.data);
                 setshow("block");
+                console.log(result);
             }
             ).catch(err => {
                 console.log(err);
@@ -49,39 +54,62 @@ export default function StockQuantity() {
                     <TopContainer heading1="Stock" heading2="Stock Keeper" addnewbtntext="Add New" path="addnew" />
                     <div className="h-full flex items-center justify-center mx-2 ">
                         <div className="h-full w-10/12 py-8">
-                            <div className="p-2 float-left">
-                                    {/* <FormInput lable=" First Name" value={props.firstname} onchange={props.onchangefirstname}/> */}
-                                    <h1 className="font-primary mb-8 text-xl font-semibold">Item Name</h1>
-                                    <input type="text" name="item" id="name" className=" rounded-lg shadow-lg h-10 w-96 float-right border-0" onChange={getByName}
-                                    />
-                                    <div className="h-auto z-20 ">
-                                        <ul class={`bg-white border border-gray-100 w-full mt-12 ${show} `} >
-                                            {result.map(item => (<li class="pl-1 pr-2 py-1 border-b-2 border-gray-100 relative cursor-pointer hover:bg-yellow-50 hover:text-gray-900" onClick={showdetails} id={item.itemNo} >
-                                                {item.itemName}
-                                            </li>))}
-                                            {(result.length == 0) ? ("Item Not found. Please Add New Item!") : ("")}
-                                        </ul>
-                                    </div>
+                            <div className=" flex">
+                                        <div className="p-2 float-left flex">
+                                            {/* <FormInput lable=" First Name" value={props.firstname} onchange={props.onchangefirstname}/> */}
+                                            <h1 className="font-primary mt-2 mb-4 text-xl font-semibold">Item Name</h1>
+                                            <input type="text" name="item" id="name" className=" rounded-lg shadow-lg h-10 w-96 float-right border-0 ml-10" onChange={getByName}
+                                            />
+                                        </div>
+                                        {/* <div className="h-auto z-20 ">
+                                            <ul class={`bg-white border border-gray-100 w-full mt-12 ${show} `} >
+                                                {result.map(item => (<li class="pl-1 pr-2 py-1 border-b-2 border-gray-100 relative cursor-pointer hover:bg-yellow-50 hover:text-gray-900" onClick={showdetails} id={item.itemNo} >
+                                                    {item.itemName}
+                                                </li>))}
+                                                {(result.length == 0) ? ("Item Not found. Please Add New Item!") : ("")}
+                                            </ul>
+                                        </div> */}
+                                    
                             </div>
                             <div className=" flex">
                                 <div className="ml-10 font-primary text-xl font-semibold">Select Category </div>
-                                <div className="ml-80"><ButtonRedProps name="Clear" link={""} /></div>
+                                {/* <div className="ml-80"><ButtonRedProps name="Clear" link={""} /></div> */}
                             </div>
-                            <div className="ml-20 p-2 rounded-lg mt-4 ">
+                            <div className="p-2 rounded-lg mt-4 ">
                                 <CategorySelectNavBarMol />
                             </div>
                             
                             <div className="font-primary text-xl">Stock Items</div>
-                            <div className="w-full h-2/5 overflow-auto">
-                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Piston" quantity="123" link={""} /></div>
-                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Brake Pad" quantity="123" link={""} /></div>
+                            <div className="w-full h-3/5 overflow-auto">
+                                <div className="h-auto z-20 ">
+                                    <ul class={` ${show} `} >
+                                                {result.map(item => (<div className="mt-4 border-b-2 pr-9 pl-12" onChange={showdetails} id={item.itemNo}>
+                                                    {/* {item.itemName} */}
+                                                     <ItemContainer3 itemNo={item.itemName} quantity={item.stock} link={""} />
+                                                     
+                                                </div>
+                                                ))}
+                                                {(result.length == 0) ? ("Item Not found. Please Add New Item!") : ("")}
+                                    </ul>
+                                </div>
+                                {/* <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Piston" quantity="123" link={""} /></div> */}
+                                {/* <div className="mt-4 border-b-2 pr-9 pl-12">
+                                    <ItemContainer3 itemNo="Brake Pad" quantity="123" link={""} />
+                                </div>
                                 <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Piston" quantity="123" link={""} /></div>
                                 <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Cluch Pad" quantity="123" link={""} /></div>
+                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Pist" quantity="123" link={""} /></div>
+                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Bake Pad" quantity="123" link={""} /></div>
                                 <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Piston" quantity="123" link={""} /></div>
-                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Brake Pad" quantity="123" link={""} /></div>
+                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Clch Pad" quantity="123" link={""} /></div>
+                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Pisto" quantity="123" link={""} /></div>
                                 <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Piston" quantity="123" link={""} /></div>
                                 <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Cluch Pad" quantity="123" link={""} /></div>
-                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Piston" quantity="123" link={""} /></div>
+                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Pistn" quantity="123" link={""} /></div>
+                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Brae Pad" quantity="123" link={""} /></div>
+                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Pison" quantity="123" link={""} /></div>
+                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="Cluch Pad" quantity="123" link={""} /></div>
+                                <div className="mt-4 border-b-2 pr-9 pl-12"><ItemContainer3 itemNo="on" quantity="123" link={""} /></div> */}
 
                             </div>
                         </div>
