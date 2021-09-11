7262892a53baacee1dddac372bedd7d7f209e818
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-09-11 14:56:19 +0530

    ReportV3

--- src/components/Atoms/admin/ReportTopNavBarAtom.js
+++ src/components/Atoms/admin/ReportTopNavBarAtom.js
@@ -5,12 +5,11 @@ import ReportTopNavbarBtn from './ReportTopNavbarBtn';
 
 function ReportTopNavBarAtom(props) {
     const [list, setList] = useState([
-        { id: 1, value: "mdmin" },
-        { id: 2, value: "Service Advisor" },
-        { id: 3, value: "Lead Technician" },
-        { id: 4, value: "Cashier" },
-        { id: 5, value: "Stock Keeper" },
-        { id: 6, value: "Technician" },
+        { id: 1, value: "Sales 1" },
+        { id: 2, value: "Sales 2" },
+        { id: 3, value: "Sales 3" },
+        { id: 4, value: "Sales 4" },
+        { id: 5, value: "Sales 5" },
     ])
 
     const [highBtn, setHighBtn] = useState(list[0].id);
@@ -20,7 +19,7 @@ function ReportTopNavBarAtom(props) {
     //console.log("CHILD"+highBtn);
 
     return (
-        <div className="grid grid-cols-3 gap-2 my-8 mx-4 place-items-center ">
+        <div className="grid grid-cols-1 gap-2 my-8 mx-4 place-items-center sm:grid-cols-3 md:grid-cols-5">
             {/* Amodgen ahanna one */}
             {/* <RemmovenavbarAdminbtn  btnName="Service"  /> */}
             {list.map(btn => <ReportTopNavbarBtn setHighBtn={setHighBtn} highBtn={highBtn} key={btn.id} btnName={btn.value} id={btn.id} />)}
--- src/components/Organs/admin/ReportTopNavbar.js
+++ src/components/Organs/admin/ReportTopNavbar.js
@@ -4,7 +4,7 @@ import ReportTopNavBarAtom from '../../Atoms/admin/ReportTopNavBarAtom'
 export default function ReportTopNavbar(props) {
     const [userType, setUserType] = useState(1);
     return (
-        <div className="bg-white shadow-xl rounded-lg w-full ">
+        <div className="bg-white shadow-xl rounded-lg w-full h-28 overflow-auto md:overflow-hidden">
             <ReportTopNavBarAtom setUserType={setUserType} userType={userType} />
         </div>
     )
