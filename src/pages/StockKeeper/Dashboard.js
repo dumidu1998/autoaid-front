9a320fcfa8b189c5641999b2068f1cc6f9bf06ea
Madhawa Dissanayake <73326224+MadhawaDissanayake@users.noreply.github.com>
2021-09-13 11:07:14 +0530

    low quantity in dashboard added with color notations

--- src/pages/StockKeeper/Dashboard.js
+++ src/pages/StockKeeper/Dashboard.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import SideNavBar from '../../components/Moleculars/stockKeeper/SideNavBar'
 import TopContainer from '../../components/Atoms/serviceStation/TopContainer'
 import ItemContainer from '../../components/Atoms/stockKeeper/ItemContainer'
 import SubSectionHeading from '../../components/Atoms/serviceStation/SubSectionHeading'
 import LowQuantityItems from '../../components/Atoms/stockKeeper/LowQuantityItems'
+import { getCookie } from '../../jsfunctions/cookies'
 
 export default function Dashboard() {
+    const [output, setoutput] = useState({itemName:"",itemNo:"",stock:"",price:"",reorderLevel:""});
+    const [result, setresult] = useState([]);
+    const [show, setshow] = useState("hidden");
+
+    useEffect(() => {
+    
+    
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/inventory/items`)
+        .then(res => {
+            setresult(res.data);
+            // setshow("block");
+            console.log(result);
+        }
+        ).catch(err => {
+            console.log(err);
+            setresult([]);
+        })
+        // setshow("hidden");
+        // setresult(res.data);
+        // setshow("block");
+
+
+
+}, [])
+
     return (
         <div className="relative bg-Background-0">
             <div className="flex flex-row">
@@ -30,17 +57,41 @@ export default function Dashboard() {
                         </div>
                         <div className="h-full w-5/12 py-8 ml-20">
                             <SubSectionHeading heading="Low Quantity Items" />
-                            <div className="bg-white w-full h-4/6 rounded-xl shadow-xl  py-12">
-                                <div className="px-4">
-                                    <LowQuantityItems itemNo="Item Number" itemName="Item Name" CurrentStatus="Current Status" />
+                            <div className="bg-white w-full rounded-xl shadow-xl py-4 pb-6">
+                                <div className="px-4 font-bold border-b-2 text-lg">
+                                    <LowQuantityItems itemNo="Item Number" itemName="Item Name" stock="Stock" />
                                 </div>
-                                <div className="w-full h-64 overflow-auto">
-                                    <div className="mt-4 border-b-2 pr-9 pl-12"><LowQuantityItems itemNo="12345" itemName="Brake Pad" CurrentStatus="Completed" /></div>
-                                    <div className="mt-4 border-b-2 pr-9 pl-12"><LowQuantityItems itemNo="15948" itemName="Brake Pad" CurrentStatus="On service" /></div>
-                                    <div className="mt-4 border-b-2 pr-9 pl-12"><LowQuantityItems itemNo="21356" itemName="Brake Pad" CurrentStatus="Completed" /></div>
-                                    <div className="mt-4 border-b-2 pr-9 pl-12"><LowQuantityItems itemNo="15943" itemName="Brake Pad" CurrentStatus="On service" /></div>
+                                <div className="w-full h-84 overflow-auto ">
+                                    {/* <ul class={` ${show} `} > */}
+                                        {result.map(item => (<div className=" pr-9 pl-12 ">
+                                                {(item.stock<(item.reorderLevel/2)) ?
+                                                    <div className="mt-4 text-red-600 text-sm font-semibold"><LowQuantityItems itemNo={item.itemNo} itemName={item.itemName} stock={item.stock} color="text-red-600"/></div>
+                                                :
+                                                    ("")
+                                                }                                                         
+                                                </div>
+                                                ))} 
+                                                
+                                    {/* </ul> */}
+                                    {/* <div className="mt-4 border-b-2 pr-9 pl-12"><LowQuantityItems itemNo={item.itemNo} itemName={item.itemNo} stock="Completed" /></div> */}
+                                    {/* <div className="mt-4 border-b-2 pr-9 pl-12"><LowQuantityItems itemNo="15948" itemName="Brake Pad" stock="On service" /></div>
+                                    <div className="mt-4 border-b-2 pr-9 pl-12"><LowQuantityItems itemNo="21356" itemName="Brake Pad" stock="Completed" /></div>*/}
+                                    {/* <div className="mt-4 border-b-2 pr-9 pl-12"><LowQuantityItems itemNo="15943" itemName="Brake Pad" stock="On service" /></div>  */}
 
 
+                                </div>
+                                <div className="w-full h-84 overflow-auto ">
+                                    {/* <ul class={` ${show} `} > */}
+                                        {result.map(item => (<div className=" pr-9 pl-12 ">
+                                                {(item.stock<(item.reorderLevel/2)) ?
+                                                    ("")
+                                                :(item.stock<item.reorderLevel) ?
+                                                    <div className="mt-4 text-yellow-500 text-sm font-semibold"><LowQuantityItems itemNo={item.itemNo} itemName={item.itemName} stock={item.stock} color="text-red-600"/></div>
+                                                :
+                                                    ("")
+                                                }                                                         
+                                                </div>
+                                                ))} 
                                 </div>
                             </div>
                         </div>
