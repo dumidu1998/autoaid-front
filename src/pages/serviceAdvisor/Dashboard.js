2b81a38924b5f5ab527cb05ac5c85169655f0468
Hasantha96 <nphlakshan@gmail.com>
2021-09-17 14:04:01 +0530

    service advisor upcoming appointments

--- src/pages/serviceAdvisor/Dashboard.js
+++ src/pages/serviceAdvisor/Dashboard.js
@@ -10,8 +10,12 @@ import { getCookie } from '../../jsfunctions/cookies'
 
 export default function Dashboard() {
     const [ongoingRepairs, setongoingRepairs] = useState([]);
-    const userId=getCookie('userId');
-    
+    const [upcomingAppointments, setupcomingAppointments] = useState([{"vehicleNumber": ""}]);
+    const [ongoingError, setongoingError] = useState('');
+    const [upcomingError, setupcomingError] = useState('');
+
+    const userId = getCookie('userId');
+
     var config = {
         headers: {
             'Authorization': 'Bearer ' + getCookie('token'),
@@ -20,11 +24,25 @@ export default function Dashboard() {
 
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_BASE_URL}/advisor/repairs/ongoing/${userId}`, config)
-            .then(function (response){
-                // console.log(response.data);
+            .then(function (response) {
+                console.log(response.data);
                 setongoingRepairs(response.data);
             })
+            .catch(function (error) {
+                console.log(error.response.data);
+                setongoingError(error.response.data);
+             })
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/advisor/appointments/today/${userId}`, config)
+            .then(function (response) {
+                console.log(response.data);
+                setupcomingAppointments(response.data);
+            })
+            .catch(function (error) {
+                console.log(error.response.data);
+                setupcomingError(error.response.data);
+             })
     }, [])
+
     return (
         <div className=" bg-Background-0 h-screen">
             <div className="flex flex-row">
@@ -35,25 +53,16 @@ export default function Dashboard() {
                     <TopContainer heading1="Dashboard" heading2="Service Advisor" addnewbtntext="Add New" path="/serviceadvisor/detailsform"/>
                     <div className="flex justify-between">
                         <div className="w-full flex flex-col items-center justify-center">
-                            <div className=" w-10/12 pt-24">
+                            <div className={upcomingError==''?"w-10/12 pt-24":" hidden"}>
                                 <SubSectionHeading heading="Next Appointment" />
-                                <AppointmentContainer vehicleNo="CAM - 9044" link="" />
+                                <AppointmentContainer vehicleNo={upcomingAppointments[0].vehicleNumber} link="/serviceadvisor/detailsform" />
                             </div>
                             <div className="pt-16 w-10/12 h-96">
                                 <SubSectionHeading heading="Upcoming Appointment" />
-                                <div className="bg-Background-0  h-full overflow-auto ">
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
-                                    <AppointmentContainer vehicleNo="CAM - 9044" link=""/>
+                                <div className={upcomingError==''?"hidden":" ml-48 mt-20 font-bold text-red-600"}>{upcomingError}</div>
+
+                                <div className={upcomingError==''?"bg-Background-0  h-full overflow-auto ":" hidden"}>
+                                    {upcomingAppointments.map(appointment=><AppointmentContainer vehicleNo={appointment.vehicleNumber} link="/serviceadvisor/detailsform"/>)}                                                                        
                                 </div>
                             </div>
                         </div>
@@ -61,10 +70,14 @@ export default function Dashboard() {
                             <SubSectionHeading heading="Ongoing Service Details" />  
                             <div className="bg-white w-full h-5/6 rounded-xl shadow-xl  py-12">
                                 <div className="px-12">
-                                    <OngoingServiceDetails RegNum="Reg. No" CurrentStatus="Current Status"/>
+                                    <div className="flex items-center justify-between w-full mt-8">
+                                        <p className="font-primary text-sm text-left">Vehicle Number</p>
+                                        <p className="font-primary text-sm">Status</p>
+                                    </div>
                                 </div>
                                 <div className="w-full h-64 overflow-auto">
-                                    {ongoingRepairs.map(repair=><div className="mt-4 border-b-2 pr-16 pl-12"><OngoingServiceDetails RegNum={repair.vehicleNumber} CurrentStatus={repair.status}/></div>)}                                                                          
+                                    <div className={ongoingError==''?"hidden":" ml-48 mt-20 font-bold text-red-600"}>{ongoingError}</div>
+                                    {ongoingRepairs.map(repair => <div className="mt-4 border-b-2 pr-16 pl-12"><OngoingServiceDetails repair={repair}/></div>)}                                                                          
                                 </div>
                             </div>
                         </div>
