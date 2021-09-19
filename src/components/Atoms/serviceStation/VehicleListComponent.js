37aa5b08361a188a72a743acff130e3c8853b938
manthila1997 <49613631+manthila1997@users.noreply.github.com>
2021-09-19 09:46:55 +0530

    update

--- src/components/Atoms/serviceStation/VehicleListComponent.js
+++ src/components/Atoms/serviceStation/VehicleListComponent.js
@@ -4,19 +4,22 @@ import Button from '../Button'
 
 export default function VehicleListComponent(props) {
     return (
-        <div className=" grid grid-cols-1 place-items-center mt-8 mb-4">
+        <div className=" grid grid-cols-1 place-items-center mb-2">
             <div className=" w-9/12 h-14 shadow-lg bg-white mt-5 rounded-lg grid grid-cols-1 place-items-center">
-                <div className="grid grid-cols-3 place-items-center w-full">
+                <div className="grid grid-cols-2 place-items-center w-full">
                     <h1 className="font-bold font-primary text-gray-400">{props.vehicle.vehicleNumber}</h1>
-                    <Button link={props.link}  txt="Update" />
-                    <ButtonSecond link={{
-                        pathname:'/serviceadvisor/section/selection',
-                        state:{
-                            vehicleNo:props.vehicle.vehicleNumber,
-                            vin:props.vehicle.vin,
-                            userId:props.userId
-                            }
-                        }} />
+                    <div className="flex justify-around items-center w-60">
+                        <Button link={props.link}  txt="Update" />
+                        <ButtonSecond link={{
+                            pathname:'/serviceadvisor/section/selection',
+                            state:{
+                                vehicleNo:props.vehicle.vehicleNumber,
+                                vin:props.vehicle.vin,
+                                userId:props.userId
+                                }
+                            }} />
+
+                    </div>
                     
                 </div>
             </div>
