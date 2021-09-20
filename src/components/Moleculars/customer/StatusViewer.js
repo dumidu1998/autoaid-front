7d75e5d16b6e105c03c5f3a9683e00c606fd9950
dumidu1998 <dumiduraj@gmail.com>
2021-09-21 03:53:12 +0530

    status done

--- src/components/Moleculars/customer/StatusViewer.js
+++ src/components/Moleculars/customer/StatusViewer.js
@@ -1,24 +1,28 @@
 import React from 'react'
 
 
-export default function StatusViewer() {
+export default function StatusViewer(props) {
+    let format = (n) => `0${n / 60 ^ 0}`.slice(-2) + '.' + ('0' + n % 60).slice(-2) + ' '
+
     return (
         <div>
             {/* Mobile view */}
             <div className="flex flex-col items-center bg-forth-0 py-5 rounded-lg mb-5 shadow-xl md:hidden relative">
                 <h1 className=" font-secondary text-white text-lg">Current Progress</h1>
-                <p className="font-primary text-white text-xs my-1">CAM-9044</p>
-                <h1 className="font-primary text-white text-sm font-semibold">Est. Time : 4.30hr</h1>
+                <p className="font-primary text-white text-xs my-1">{props.data.vehicleNumber}</p>
+                <h1 className="font-primary text-white text-sm font-semibold">Est. Time : {format(props.data.estTime)}hr</h1>
                 <img className=" w-44 h-44 rounded-full my-5 border-2 border-primary-0" src="/imgs/Icons/repair.png" />
                 <h1 className="font-primary text-white text-sm font-semibold">On Service</h1>
                 <a href="/customer/checklist" className="absolute top-2 right-5 font-primary text-xs text-Secondary-0">View Check List</a>
 
-                <div className="w-1/3 flex my-1 items-center justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary-0" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-                    </svg>
-                    <p className="font-primary text-white text-xs my-1 ml-6 text-center">Car Wash</p>
-                </div>
+                {props.data.ongoing.map((it, index) => (
+                    <div className="w-1/3 flex my-1 items-center justify-center" key={index}>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary-0" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        </svg>
+                        <p className="font-primary text-white text-xs my-1 ml-6 text-center">{it}</p>
+                    </div>
+                ))}
 
 
             </div>
@@ -26,8 +30,8 @@ export default function StatusViewer() {
             {/* web view */}
             <div className="md:flex flex-col items-center bg-forth-0 py-5 rounded-lg mb-5 shadow-xl hidden relative">
                 <h1 className=" font-secondary text-white text-lg">Current Progress</h1>
-                <p className="font-primary text-white text-xs my-1">CAM-9044</p>
-                <h1 className="font-primary text-white text-sm font-semibold">Est. Time : 4.30hr</h1>
+                <p className="font-primary text-white text-xs my-1">{props.data.vehicleNumber}</p>
+                <h1 className="font-primary text-white text-sm font-semibold">Est. Time : {format(props.data.estTime)}hr</h1>
                 <div className="w-10/12 flex items-center justify-between">
                     <div className="">
                         <img className=" w-44 h-44 rounded-full my-5 border-2 border-primary-0" src="/imgs/Icons/diognize.png" />
@@ -45,16 +49,16 @@ export default function StatusViewer() {
 
                     </div>
 
-
                     <a href="/customer/checklist" className="absolute top-5 right-5 font-primary text-xs text-Secondary-0">View Check List</a>
                 </div>
-                <div className="w-1/3 flex my-1 items-center justify-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary-0" viewBox="0 0 20 20" fill="currentColor">
-                        <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
-                    </svg>
-                    <p className="font-primary text-white text-xs my-1 ml-6 text-center">Car Wash</p>
-                </div>
-
+                {props.data.ongoing.map((it, index) => (
+                    <div className="w-1/3 flex my-1 items-center justify-center" key={index}>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary-0" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        </svg>
+                        <p className="font-primary text-white text-xs my-1 ml-6 text-center">{it}</p>
+                    </div>
+                ))}
 
             </div>
 
