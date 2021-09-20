71d974846671ade9c4ef666e80590dc986660782
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-09-20 20:45:24 +0530

    navbar

--- src/pages/admin/AdminSection.js
+++ src/pages/admin/AdminSection.js
@@ -31,7 +31,7 @@ export default function AdminSection() {
             <div className="md:ml-40 "><AdminSideBar name="Section Manage " roleName="Admin" /></div>
             <div className=" w-full absolute  bg-Background-0 ">
                 <div className="md:ml-40">
-                    <div className="font-primary text-xl font-medium ml-6 mt-8 md:text-2xl lg:text-3xl xl:text-4xl">Sections</div>
+                    <div className="font-primary text-xl font-medium ml-6 mt-8 md:text-2xl lg:text-3xl xl:text-4xl text-white">Sections</div>
                     <div className="px-3 md:px-8 -mt-24">
                         <div className="container mx-auto max-w-full">
                             <div className="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3">
