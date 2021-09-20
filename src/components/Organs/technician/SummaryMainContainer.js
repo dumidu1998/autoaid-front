a02a563b9f37e2c27c7e64aaddb7c700edd9c3f4
Hasantha96 <nphlakshan@gmail.com>
2021-09-20 10:17:58 +0530

    set tech to service entry

--- src/components/Organs/technician/SummaryMainContainer.js
+++ src/components/Organs/technician/SummaryMainContainer.js
@@ -31,14 +31,12 @@ export default function SummaryMainContainer(props) {
         <div className="bg-white  p-3 shadow-xl rounded-lg w-full mr-10 my-10">
             <SubSectionHeading heading="Repair List" />
             <div className=" flex flex-col items-center">
-                {serviceEntryList.map(entry=><RepairList title={entry.subCatName} time={entry.estimatedTime} />)}
-{/*                 
-                <RepairList title="Engine Piston Repair" time="20 mins" />
-                <RepairList title="Engine Piston Repair" time="20 mins" /> */}
+                {serviceEntryList.map(entry=><RepairList title={entry.subCatName} time={entry.estimatedTime} />)}                
+                {/* <RepairList title="Engine Piston Repair" time="20 mins" /> */}
             </div>
             <SubSectionHeading heading="Available Technicians" />
             <div className=" flex flex-col items-center">
-                <TechniciansDropDown url1="/admin/getstaff/3" url2="/admin/gettech/" url3="/admin/settech/" />
+                <TechniciansDropDown url1="/admin/getstaff/6" url2={"/technician/getTech/"+props.repair.repairId+"/"} url3={"/technician/assignTech/"+props.repair.repairId+"/"} defaultMsg="Assign Tech" sectionName={props.sectionName} />
             </div>
         </div>
     )
