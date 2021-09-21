0c08fd2287590c2f47ccbedaaad9031491b6f099
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-09-21 17:50:30 +0530

    PopUpV7

--- src/components/Atoms/admin/PopBtnOne.js
+++ src/components/Atoms/admin/PopBtnOne.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function PopBtnOne(props) {
-    let btnStyle="font-primary w-16 h-10 bg-blue-400 rounded-lg shadow-xl text-white ";
+    let btnStyle="font-primary w-16 h-10 rounded-lg shadow-xl text-white ";
     btnStyle+=props.popUpBtnOneColor;
     return (
         <div>
