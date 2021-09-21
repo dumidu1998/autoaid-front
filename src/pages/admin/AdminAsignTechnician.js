b24edb117d6c21863fd78cdf95814c6839dc7e64
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-09-21 14:56:47 +0530

    update2

--- src/pages/admin/AdminAsignTechnician.js
+++ src/pages/admin/AdminAsignTechnician.js
@@ -29,10 +29,10 @@ export default function AdminAsignTechnician(props) {
             <div className="md:ml-40 bg-Background-0"><AdminSideBar name="Slot Manage " roleName="Admin" /></div>
             <div className=" w-full absolute  bg-Background-0 ">
                 <div className="md:ml-40">
-                    <div className="font-primary text-xl font-medium ml-12   mt-8 md:text-2xl lg:text-3xl xl:text-4xl">{name}</div>
+                    <div className="font-primary text-xl font-medium ml-12   mt-8 md:text-2xl lg:text-3xl xl:text-4xl text-white">{name}</div>
                     <div className="px-3 md:px-8 -mt-24">
                         <div className="container mx-auto max-w-full">
-                            <div className="flex justify-center items-center mt-32 md:mt-16 md:justify-end md:mr-10"><AddSlotBtn /></div>
+                            <div className="flex justify-center items-center mt-32 sm:mt-20 md:mt-4 md:justify-end md:mr-10"><AddSlotBtn /></div>
                             <div className="bg-white shadow-xl rounded-lg w-full px-8 py-12 mt-6 mb-6">
                                 <div className="flex justify-center items-center">
                                     <TechniciansDropDown sectionName={name} url1="/admin/getstaff/3" url2="/admin/gettech/" url3="/admin/settech/" defaultMsg="Assign Lead Tech"/>
