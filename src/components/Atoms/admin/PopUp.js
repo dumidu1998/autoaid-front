0f1f545e4884ade356b1ce11079bbd1cbd381c5b
AmdPathirana <2018is052@stu.ucsc.cmb.ac.lk>
2021-09-21 16:30:06 +0530

    PopUpV1

---
+++ src/components/Atoms/admin/PopUp.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./PopUp.css";
+
+export default function PopUp() {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if(modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
+  return (
+    <>
+      <button onClick={toggleModal} className="btn-modal">
+        Open
+      </button>
+
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <h2>Hello Modal</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+              perferendis suscipit officia recusandae, eveniet quaerat assumenda
+              id fugit, dignissimos maxime non natus placeat illo iusto!
+              Sapiente dolorum id maiores dolores?
+            </p>
+            <button className="close-modal bg-red-400 text-sm" onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
+      
+    </>
+  );
+}
