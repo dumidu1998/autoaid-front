1db4c6526f2c72fbcd6f78ea7f41895003bf88d4
dumidu1998 <dumiduraj@gmail.com>
2021-09-21 12:05:53 +0530

    checklist done customer

--- src/pages/customer/CheckListPage.js
+++ src/pages/customer/CheckListPage.js
@@ -4,6 +4,7 @@ import SubSectionHeading from '../../components/Atoms/serviceStation/SubSectionH
 import TopNav from '../../components/Moleculars/customer/TopNav'
 import { Link, useHistory, useLocation } from 'react-router-dom/cjs/react-router-dom.min'
 import axios from 'axios';
+import { db } from '../../Firebase'
 
 
 var config = {
@@ -16,78 +17,86 @@ var config = {
 export default function CheckListPage() {
 
     const location = useLocation();
-
-    console.log(location.state);
+    const [docid, setdocid] = useState('')
+    const [data, setdata] = useState({});
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_BASE_URL}/customer/getdocid/${location.state}`, config).then((res) => {
-            console.log(res.data);
+            setdocid(res.data);
+            return res.data
+        }).then(e => {
+            db.collection('checklist').doc(e).onSnapshot(snapshot => {
+                setdata(snapshot.data());
+            })
         })
+
     }, [])
 
     return (
         <div className="flex flex-col items-center font-primary">
             <TopNav />
-            <div className="md:w-1/2 w-full rounded-md shadow-lg h-screen my-5 flex flex-col items-center ">
+            <div className="md:w-1/2 w-full rounded-md shadow-lg my-5 py-5 flex flex-col items-center ">
                 <h1 className="font-semibold text-2xl  tracking-wider my-10">Vehicle Items Check List</h1>
-                <table className="table-auto w-3/4  text-center bg-red-400">
+                <table className="table-auto w-3/4  text-center ">
                     <tbody>
                         <tr>
-                            <td>Service Type</td>
-                            <td>Millage</td>
+                            <th>Service Type</th>
+                            <th>Millage</th>
                         </tr>
                         <tr>
-                            <td>Repair</td>
-                            <td>12345 km</td>
+                            <td>{data.serviceType}</td>
+                            <td>{data.millage} km</td>
                         </tr>
                     </tbody>
                 </table>
-                <table className="table-auto w-3/4  text-center bg-red-400 my-10">
-                    <tbody>
-                        <tr>
-                            <td>img</td>
-                            <td>front</td>
+                <table className="table-auto w-3/4  text-center bg-primary-0 my-10 ">
+                    <tbody >
+                        <tr >
+                            <th>Image 1</th>
+                            <th>Image 2</th>
                         </tr>
                         <tr>
-                            <td>img</td>
-                            <td>rear km</td>
+                            <td><img src={data.image1} /></td>
+                            <td><img src={data.image2} /></td>
                         </tr>
                     </tbody>
                 </table>
-                <table className="table-auto w-3/4  text-center bg-red-400 my-10">
+                <table className="table-auto w-3/4  text-center bg-primary-0 my-10">
                     <tbody>
                         <tr>
-                            <td>img</td>
-                            <td>front</td>
+                            <th>Image 3</th>
+                            <th>Image 4</th>
                         </tr>
                         <tr>
-                            <td>img</td>
-                            <td>rear km</td>
+                            <td><img src={data.image3} /></td>
+                            <td><img src={data.image4} /></td>
                         </tr>
                     </tbody>
                 </table>
 
-                <div className="w-3/4">
-                    <h1 className="font-semibold text-lg  tracking-wide">Description</h1>
-                    <p className=" text-sm tracking-wide">jdbisufsiuf ebfiuebfs sjbfisjebksjdf sbskjfbksjbf </p>
-                </div>
                 <div className="w-3/4 my-5">
                     <h1 className="font-semibold text-lg  tracking-wide">Items In the Vehicle</h1>
-                    <p className=" text-sm tracking-wide"> Documents</p>
-                    <p className=" text-sm tracking-wide"> Spare Wheel</p>
+
+                    {data.document ? < p className=" text-s tracking-wide"> Documents</p> : ''}
+                    {data.spare ? < p className=" text-s tracking-wide"> Spare Wheel</p> : ''}
+                    {data.tools ? < p className=" text-s tracking-wide"> Tool Set</p> : ''}
                 </div>
                 <table className="table-auto w-3/4  text-center my-10">
                     <tbody>
                         <tr>
-                            <td>Carpet Count</td>
-                            <td>Seat Covers</td>
+                            <th>Carpet Count</th>
+                            <th>Seat Covers</th>
                         </tr>
                         <tr>
-                            <td>4</td>
-                            <td>6</td>
+                            <td>{data.carpets}</td>
+                            <td>{data.seatcovers}</td>
                         </tr>
                     </tbody>
                 </table>
+                <div className="w-3/4">
+                    <h1 className="font-semibold text-lg  tracking-wide">Description</h1>
+                    <p className=" text-sm tracking-wide">{data.description == " " ? ' - ' : data.description}</p>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
