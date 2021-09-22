9791f701ecd0e71617c6fef9ea33c11cd88c5f6f
manthila1997 <49613631+manthila1997@users.noreply.github.com>
2021-09-22 15:59:09 +0530

    customer

--- src/App.js
+++ src/App.js
@@ -52,6 +52,7 @@ import PaymentInvoice from './pages/cashier/PaymentInvoice';
 import RepairInvoice from './pages/serviceAdvisor/RepairInvoice';
 import ForgetPassword from './pages/ForgetPassword';
 import ForgetPassCodeSubmit from './components/Atoms/ForgetPassCodeSubmit';
+import CustomerDetails from './pages/admin/CustomerDetails';
 
 
 
@@ -108,6 +109,7 @@ function App() {
           <Route path="/admin/test" exact component={Testing} />
           <Route path="/admin/technician/asign" exact component={AdminAsignTechnician} />
           <Route path="/admin/reports" exact component={AdminReports} />
+          <Route path="/admin/customers" exact component={CustomerDetails} />
 
           <Route path="/technician/dashboard" exact component={Dashboard} />
           {/* <Route path="/technician" exact component={Dashboard} /> */}
---
+++ src/pages/admin/CustomerDetails.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import AdminSideBar from '../../components/Moleculars/admin/AdminSideBar'
+
+
+export default function CustomerDetails() {
+    return (
+        <div>
+            <div className="md:ml-40 bg-Background-0"><AdminSideBar name="Customer Details " roleName="Admin" /></div>
+
+            <div>
+                <table>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
