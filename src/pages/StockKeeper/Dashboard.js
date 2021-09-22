84bfcecb927279c6cc521fba0e6b63e1eb16702e
dumidu1998 <dumiduraj@gmail.com>
2021-09-22 13:58:36 +0530

    modal done special request

--- src/pages/StockKeeper/Dashboard.js
+++ src/pages/StockKeeper/Dashboard.js
@@ -9,11 +9,11 @@ import { getCookie } from '../../jsfunctions/cookies'
 import { ToastContainer, toast } from 'react-toastify';
 
 export default function Dashboard() {
-    const [output, setoutput] = useState({itemName:"",itemNo:"",stock:"",price:"",reorderLevel:""});
+    const [output, setoutput] = useState({ itemName: "", itemNo: "", stock: "", price: "", reorderLevel: "" });
     const [result, setresult] = useState([]);
     const [show, setshow] = useState("hidden");
     const [request, setrequest] = useState([]);
-const [changed, setchanged] = useState(true);
+    const [changed, setchanged] = useState(true);
     // useEffect(() => {
     //     console.log("test")
     //     axios.get(`${process.env.REACT_APP_API_BASE_URL}/inventory/itemRequestAll`)
@@ -52,36 +52,36 @@ const [changed, setchanged] = useState(true);
             })
     }
 
-useEffect(() => {
-    console.log("test")
+    useEffect(() => {
+        console.log("test")
         axios.get(`${process.env.REACT_APP_API_BASE_URL}/inventory/itemRequestAll`)
-        .then(res => {
-            setrequest(res.data);
-            console.log(res.data);
-        }
-        ).catch(err => {
-            console.log(err);
-            setrequest([]);
-        })
-    
-    axios.get(`${process.env.REACT_APP_API_BASE_URL}/inventory/items`)
-    .then(res => {
-        setresult(res.data);
-        setshow("block");
-        console.log(result);
-    }
-    ).catch(err => {
-        console.log(err);
-        setresult([]);
-        console.log("error use effect");
-    })
-    // setshow("hidden");
-    // setresult(res.data);
-    // setshow("block");
+            .then(res => {
+                setrequest(res.data);
+                console.log(res.data);
+            }
+            ).catch(err => {
+                console.log(err);
+                setrequest([]);
+            })
+
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/inventory/items`)
+            .then(res => {
+                setresult(res.data);
+                setshow("block");
+                console.log(result);
+            }
+            ).catch(err => {
+                console.log(err);
+                setresult([]);
+                console.log("error use effect");
+            })
+        // setshow("hidden");
+        // setresult(res.data);
+        // setshow("block");
 
 
 
-}, [changed])
+    }, [changed])
 
     return (
         <div className="h-full w-full relative bg-Background-0">
@@ -96,27 +96,27 @@ useEffect(() => {
                             <SubSectionHeading heading="Item Requests" />
                             <div className="w-full h-full overflow-y-auto">
                                 {request.map(item => (
+                                    <div className="">
+                                        <div className=" w-full h-36 shadow-lg bg-white mt-5 rounded-lg flex items-center justify-center">
+                                            <div className="flex flex-row w-10/12 items-center justify-between">
+                                                <div>
+                                                    <h1 className="my-4 font-bold font-primary text-lg text-black">{item.itemName}</h1>
+                                                    <h1 className="my-4 font-bold font-primary text-gray-400">{item.vehicleNumber}</h1>
+                                                </div>
+                                                <div>
+                                                    <h1 className="font-bold font-primary text-2xl text-black">{item.quantity}</h1>
+                                                </div>
                                                 <div className="">
-                                                    <div className=" w-full h-36 shadow-lg bg-white mt-5 rounded-lg flex items-center justify-center">
-                                                        <div className="flex flex-row w-10/12 items-center justify-between">
-                                                            <div>
-                                                                <h1 className="my-4 font-bold font-primary text-lg text-black">{item.itemName}</h1>
-                                                                <h1 className="my-4 font-bold font-primary text-gray-400">{item.vehicleNumber}</h1>
-                                                            </div>
-                                                            <div>
-                                                                <h1 className="font-bold font-primary text-2xl text-black">{item.quantity}</h1>
-                                                            </div>
-                                                            <div className="">
-                                                                <div className="my-4 w-auto h-10 rounded-lg flex items-center justify-center bg-green-600 p-4">
-                                                                    <button className="text-lg font-primary font-medium text-white" onClick={()=>approve(item.requestId)} >Accept</button>
-                                                                </div>
-                                                                <div className="my-4 w-auto h-10 rounded-lg flex items-center justify-center bg-red-600 p-4">
-                                                                    <button className="text-lg font-primary font-medium text-white" onClick={()=>refer(item.requestId)}>Refer to Admin</button>
-                                                                </div>
-                                                            </div>
-                                                        </div>
+                                                    <div className="my-4 w-auto h-10 rounded-lg flex items-center justify-center bg-green-600 p-4">
+                                                        <button className="text-lg font-primary font-medium text-white" onClick={() => approve(item.requestId)} >Accept</button>
+                                                    </div>
+                                                    <div className="my-4 w-auto h-10 rounded-lg flex items-center justify-center bg-red-600 p-4">
+                                                        <button className="text-lg font-primary font-medium text-white" onClick={() => refer(item.requestId)}>Refer to Admin</button>
                                                     </div>
                                                 </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 ))}
                                 {/* <ItemContainer itemNo="Piston" parts="25" repair="2" link={""} />
                                 <ItemContainer itemNo="Brake Pad" link={""} />
@@ -136,15 +136,15 @@ useEffect(() => {
                                 </div>
                                 <div className="w-full h-84 overflow-auto ">
                                     {/* <ul class={` ${show} `} > */}
-                                        {result.map(item => (<div className=" pr-9 pl-12 ">
-                                                {(item.stock<(item.reorderLevel/2)) ?
-                                                    <div className="mt-4 text-red-600 text-sm font-semibold"><LowQuantityItems itemNo={item.itemNo} itemName={item.itemName} stock={item.stock} color="text-red-600"/></div>
-                                                :
-                                                    ("")
-                                                }                                                         
-                                                </div>
-                                                ))} 
-                                                
+                                    {result.map(item => (<div className=" pr-9 pl-12 ">
+                                        {(item.stock < (item.reorderLevel / 2)) ?
+                                            <div className="mt-4 text-red-600 text-sm font-semibold"><LowQuantityItems itemNo={item.itemNo} itemName={item.itemName} stock={item.stock} color="text-red-600" /></div>
+                                            :
+                                            ("")
+                                        }
+                                    </div>
+                                    ))}
+
                                     {/* </ul> */}
                                     {/* <div className="mt-4 border-b-2 pr-9 pl-12"><LowQuantityItems itemNo={item.itemNo} itemName={item.itemNo} stock="Completed" /></div> */}
                                     {/* <div className="mt-4 border-b-2 pr-9 pl-12"><LowQuantityItems itemNo="15948" itemName="Brake Pad" stock="On service" /></div>
@@ -155,17 +155,17 @@ useEffect(() => {
                                 </div>
                                 <div className="w-full h-84 overflow-auto ">
                                     {/* <ul class={` ${show} `} > */}
-                                        {result.map(item => (<div className=" pr-9 pl-12 ">
-                                            
-                                                {(item.stock<(item.reorderLevel/2)) ?
-                                                    ("")
-                                                :(item.stock<item.reorderLevel) ?
-                                                    <div className="mt-4 text-yellow-500 text-sm font-semibold"><LowQuantityItems itemNo={item.itemNo} itemName={item.itemName} stock={item.stock} color="text-red-600"/></div>
+                                    {result.map(item => (<div className=" pr-9 pl-12 ">
+
+                                        {(item.stock < (item.reorderLevel / 2)) ?
+                                            ("")
+                                            : (item.stock < item.reorderLevel) ?
+                                                <div className="mt-4 text-yellow-500 text-sm font-semibold"><LowQuantityItems itemNo={item.itemNo} itemName={item.itemName} stock={item.stock} color="text-red-600" /></div>
                                                 :
-                                                    ("")
-                                                }                                                         
-                                                </div>
-                                                ))} 
+                                                ("")
+                                        }
+                                    </div>
+                                    ))}
                                 </div>
                             </div>
                         </div>
@@ -173,17 +173,17 @@ useEffect(() => {
                 </div>
             </div>
             <ToastContainer
-                        position="bottom-right"
-                        autoClose={3000}
-                        hideProgressBar={false}
-                        newestOnTop={false}
-                        closeOnClick
-                        rtl={false}
-                        pauseOnFocusLoss
-                        draggable
-                        pauseOnHover
+                position="bottom-right"
+                autoClose={3000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
 
-                    />
+            />
         </div>
     )
 
