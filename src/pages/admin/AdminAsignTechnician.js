8ec35847eb987ea2430a40e2b7823bde012e84fb
dumidu1998 <dumiduraj@gmail.com>
2021-09-22 17:11:46 +0530

    slots issue fixed

--- src/pages/admin/AdminAsignTechnician.js
+++ src/pages/admin/AdminAsignTechnician.js
@@ -17,13 +17,14 @@ export default function AdminAsignTechnician(props) {
         }
     }
 
+    const [added, setadded] = useState(true);
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_BASE_URL}/admin/slots/getdetails/${name}`, config)
             .then(function (response) {
                 console.log(response.data);
                 setslotInfo(response.data);
             })
-    }, [])
+    }, [added])
     return (
         <div className="">
             <div className="md:ml-40 bg-Background-0"><AdminSideBar name="Slot Manage " roleName="Admin" /></div>
@@ -32,13 +33,15 @@ export default function AdminAsignTechnician(props) {
                     <div className="font-primary text-xl font-medium ml-12   mt-8 md:text-2xl lg:text-3xl xl:text-4xl text-white">{name}</div>
                     <div className="px-3 md:px-8 -mt-24">
                         <div className="container mx-auto max-w-full">
-                            <div className="flex justify-center items-center mt-32 sm:mt-20 md:mt-4 md:justify-end md:mr-10"><AddSlotBtn /></div>
+                            <div className="flex justify-center items-center mt-32 sm:mt-20 md:mt-4 md:justify-end md:mr-10">
+                                <AddSlotBtn secid={location.state.secid} added={setadded} />
+                            </div>
                             <div className="bg-white shadow-xl rounded-lg w-full px-8 py-12 mt-6 mb-6">
                                 <div className="flex justify-center items-center">
-                                    <TechniciansDropDown sectionName={name} url1="/admin/getstaff/3" url2="/admin/gettech/" url3="/admin/settech/" defaultMsg="Assign Lead Tech"/>
+                                    <TechniciansDropDown sectionName={name} url1="/admin/getstaff/3" url2="/admin/gettech/" url3="/admin/settech/" defaultMsg="Assign Lead Tech" />
                                 </div>
                                 <div className="flex flex-col justify-center items-center mt-6 sm:grid grid-cols-2 place-items-center lg:grid-cols-3 xl:grid-cols-4">
-                                    {slotInfo.map(slot => <SlotNumberCard color={slot.slotStatus == 'AVAILABLE' ? "bg-green-500" : "bg-red-500"} slotName={slot.slotName} vNum={slot.slotStatus == 'AVAILABLE' ? "AVAILABLE" : slot.assignedVehicle} techName={slot.assignedTechnicianName == null ? "Technician Not Assigned" : slot.assignedTechnicianName} />)}
+                                    {slotInfo.map(slot => <SlotNumberCard color={slot.slotStatus == 'AVAILABLE' ? "bg-green-500" : "bg-red-500"} slotName={slot.slotName} vNum={slot.slotStatus == 'AVAILABLE' ? "AVAILABLE" : slot.slotStatus == 'NOTAVAILABLE' ? "NOT AVAILABLE" : slot.assignedVehicle} techName={slot.assignedTechnicianName == null ? "Technician Not Assigned" : slot.assignedTechnicianName} />)}
                                 </div>
 
                             </div>
