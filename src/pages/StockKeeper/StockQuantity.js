8bb121c2fd82cf5ca588af5e0f5aaf2dc0efb0d6
Madhawa Dissanayake <73326224+MadhawaDissanayake@users.noreply.github.com>
2021-09-22 06:51:59 +0530

    Update StockQuantity.js

--- src/pages/StockKeeper/StockQuantity.js
+++ src/pages/StockKeeper/StockQuantity.js
@@ -148,7 +148,7 @@ useEffect(() => {
                             
                             <div className="ml-2 font-semibold text-xl text-white">Stock Items</div>
                             <div className="w-full h-full overflow-auto">
-                                    <div className="flex justify-evenly mr-60 pr-9 pl-5 text-white font-semibold">
+                                    <div className="flex justify-evenly mr-60 mb-2 pr-9 pl-5 text-white font-semibold">
                                         <h1 className="mr-20">Part Name  </h1>
                                         <h1 className="ml-20 mr-20">Quantity Available </h1>
                                     </div>
