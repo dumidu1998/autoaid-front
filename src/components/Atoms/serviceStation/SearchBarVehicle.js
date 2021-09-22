8961ff1924044d18ef1f9a7820cf97ed3464e8b2
Hasantha96 <nphlakshan@gmail.com>
2021-09-22 06:53:01 +0530

    service advisor get vehicle info completed

--- src/components/Atoms/serviceStation/SearchBarVehicle.js
+++ src/components/Atoms/serviceStation/SearchBarVehicle.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
 
-export default function SearchBarVehicle() {
-    
+export default function SearchBarVehicle(props) {
+
+    const [searchItem, setsearchItem] = useState('');
+
+    function getVehicle(){
+        if(searchItem==''){
+            props.setresetTable(!props.resetTable)
+        }else{
+            // console.log(searchItem);
+            if(props.vehicleList.some(vehicle=>vehicle.vehicleNumber==searchItem)){
+                toast.success("✔  Vehicle Found")
+                props.setvehicleList([props.vehicleList.find(vehicle=>vehicle.vehicleNumber==searchItem)]);
+                
+            }else{
+                toast.error('❌ Vehicle Is Not in the List');
+                // console.log("Not Have");
+                
+            }
+                        
+        }
+    }
     return (
         <div className="w-full flex justify-center mt-8 ">
             <div class="bg-white shadow p-4 flex sm:w-9/12 md:w-10/12  lg:w-11/12 max-w-3xl rounded-lg">
-                <input class=" focus:ring-white focus:border-transparent focus-border-none rounded w-full  p-2 font-primary py-2 px-13 border-0 border-b-2" type="text" placeholder="Vehicle Number (PC-4078)" />
-                <button class="  bg-gradient-to-r from-red-400 to-yellow-500 rounded text-white p-2 pl-4 pr-4 ml-2 w-20 md:w-32 lg:w-40 xl:w-56">
+                <input onChange={(e)=>setsearchItem(e.target.value)} class=" focus:ring-white focus:border-transparent focus-border-none rounded w-full  p-2 font-primary py-2 px-13 border-0 border-b-2" type="text" placeholder="Vehicle Number (PC-4078)" />
+                <button onClick={getVehicle} class="  bg-gradient-to-r from-red-400 to-yellow-500 rounded text-white p-2 pl-4 pr-4 ml-2 w-20 md:w-32 lg:w-40 xl:w-56">
                 <p class=" font-primary font-semibold lg:text-lg">Search</p>
                 </button>
             </div>
--- src/pages/serviceAdvisor/SearchVehicle.js
+++ src/pages/serviceAdvisor/SearchVehicle.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import AdminTopBar from '../../components/Atoms/admin/AdminTopBar'
 import EndPath from '../../components/Atoms/EndPath'
 import SearchVihicleRowOne from '../../components/Atoms/SearchVihicleRowOne'
@@ -8,7 +8,28 @@ import SearchBarVehicle from '../../components/Atoms/serviceStation/SearchBarVeh
 import StartPath from '../../components/Atoms/StartPath'
 import SideNav from '../../components/Moleculars/serviceAdvisor/sideNav'
 import VehicleListTable from '../../components/Organs/serviceAdvisor/VehicleListTable'
+import axios from 'axios'
+import { getCookie } from '../../jsfunctions/cookies'
+import { ToastContainer } from 'react-toastify'
 export default function SearchVehicle() {
+    const [vehicleList, setvehicleList] = useState([]);
+    const [resetTable, setresetTable] = useState(true);
+    const userId=getCookie('userId');
+    var config = {
+        headers: {
+            'Authorization': 'Bearer ' + getCookie('token'),
+        }
+    }
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_API_BASE_URL}/advisor/getAllRepairs/${userId}`, config)
+            .then(function (response) {
+                console.log(response.data);
+                setvehicleList(response.data);
+            })
+            .catch(function (error) {
+                console.log(error.response.data);
+            });
+    }, [resetTable])
     return (
             <div className=" bg-Background-0  h-screen ">
             <div className="flex flex-row">
@@ -16,17 +37,29 @@ export default function SearchVehicle() {
                     <SideNav />
                 </div>
                 <div className="w-full flex flex-col xl:ml-40">
-                    <AdminTopBar name="XXXXXXXX" roleName="Service Advisor" />
+                    <AdminTopBar name="Vehicle Info" roleName="Service Advisor" />
                     <div className="container mx-auto max-w-full overflow-hidden">
                         <div className="mt-20 mb-12">
-                            <SearchBarVehicle/>
+                            <SearchBarVehicle resetTable={resetTable} setresetTable={setresetTable}  vehicleList={vehicleList} setvehicleList={setvehicleList}/>
                         </div>
                         <div className="mb-8 w-11/12 grid grid-cols-1 place-items-center mt-32 ml-3 lg:ml-12 xl:ml-14">
-                            <VehicleListTable/>
+                            <VehicleListTable vehicleList={vehicleList}/>
+                            {/* {vehicleList.find()} */}
                         </div>
                     </div>
                 </div>
             </div>
+            <ToastContainer
+            position="bottom-right"
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            />
             </div>
     )
 }
