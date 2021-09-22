e4d5cd64a505f67490ecbeb7d8caf0af9c3c80ca
Madhawa Dissanayake <73326224+MadhawaDissanayake@users.noreply.github.com>
2021-09-22 20:01:49 +0530

    Update CashierVehicleList.js

--- src/components/Organs/cashier/CashierVehicleList.js
+++ src/components/Organs/cashier/CashierVehicleList.js
@@ -3,6 +3,20 @@ import CardHeader from '@material-tailwind/react/CardHeader';
 import CardBody from '@material-tailwind/react/CardBody';
 
 export default function CashierVehicleList(props) {
+
+    // function customer({vehicle.ve}) {
+    //     console.log(e);
+    //     axios.put(`${process.env.REACT_APP_API_BASE_URL}/inventory/approveItemRequest/${v}`, config)
+    //         .then(res => {
+    //             console.log(res.data);
+    //             setchanged(!changed);
+    //             toast.success(" Approved Successfully");
+    //         }
+    //         ).catch(err => {
+    //             console.log(err);
+    //         })
+    // }
+
     return (
         <Card className="mb-12">
             <CardHeader className="bg-gradient-to-r from-red-400 to-yellow-500" contentPosition="none">
@@ -15,21 +29,21 @@ export default function CashierVehicleList(props) {
                     <table className="items-center w-full bg-transparent border-collapse overflow-auto">
                         <thead className="">
                             <tr>
-                            <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-bold font-primary text-left">
+                            <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-bold font-primary text-center">
                                     Vehicle Number
                                 </th>
-                                <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-bold font-primary text-left">
+                                <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-bold font-primary text-center">
                                     Contact Number
                                 </th>
-                                <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-bold font-primary text-left">
+                                <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-bold font-primary text-center">
                                     Customer Name
                                 </th>
-                                <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-bold font-primary text-left">
-                                    Amount
+                                <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-bold font-primary text-center">
+                                    
                                 </th>
-                                <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-bold font-primary text-left">
+                                {/* <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-bold font-primary text-left">
                                     Completed
-                                </th>
+                                </th> */}
                                 {/* <th className="px-2 text-teal-500 align-middle border-b border-solid border-gray-200 py-3 text-sm whitespace-nowrap font-bold font-primary text-left">
                                     Status
                                 </th>
@@ -42,21 +56,21 @@ export default function CashierVehicleList(props) {
                             {props.vehicleList.map(vehicle=>
 
                             <tr className="font-primary ">
-                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                                    {vehicle.vehicleNumber}{vehicle.vin}
-                                </th>
-                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
                                     {vehicle.vehicleNumber}
                                 </th>
-                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                <th className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
+                                    {vehicle.vehicleNumber}
+                                </th>
+                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
                                     {vehicle.repairAddedDate}
                                 </td>
-                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
+                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-center">
+                                    <button class=" bg-blue-700 rounded text-white p-2 pl-4 pr-4 ml-2 ">Continue</button>
+                                </td>
+                                {/* <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
                                     {vehicle.amount}
-                                </td>
-                                <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
-                                    <button class=" bg-green-700 rounded text-white p-2 pl-4 pr-4 ml-2 w-15 md:w-20 lg:w-20 xl:w-20">Paid</button>
-                                </td>
+                                </td> */}
                                 {/* <td className="border-b border-gray-200 align-middle font-light text-sm whitespace-nowrap px-2 py-4 text-left">
                                     {vehicle.status}
                                 </td>
