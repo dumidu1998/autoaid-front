0e46d92b6823fa68088bb1841a50c5a9520d49e4
Hasantha96 <nphlakshan@gmail.com>
2021-09-22 19:01:20 +0530

    user name on login

--- src/components/Atoms/admin/AdminTopBar.js
+++ src/components/Atoms/admin/AdminTopBar.js
@@ -1,10 +1,28 @@
-import {useState } from 'react';
+import {useState, useEffect } from 'react';
 import DashboardName from './DashboardName';
 import Profileicon from './Profileicon';
 import Button from '@material-tailwind/react/Button';
 import Icon from '@material-tailwind/react/Icon';
+import axios from 'axios';
+import { getCookie } from '../../../jsfunctions/cookies';
 export default function AdminTopBar(props) {
     const [showSidebar, setShowSidebar] = useState('-left-64');
+    const [userName, setuserName] = useState('User');
+    const userId = getCookie('userId');
+    const userType = getCookie('userType');
+    var config = {
+        headers: {
+            'Authorization': 'Bearer ' + getCookie('token'),
+        }
+    }
+    useEffect(() => {
+        axios.post(`${process.env.REACT_APP_API_BASE_URL}/auth/getUserName`, { "userId": userId, "userType": userType }, config)
+            .then(function (response) {
+                console.log(response.data);
+                
+                setuserName(response.data);
+            })
+    }, [])
     return (
 
         <>
@@ -12,7 +30,7 @@ export default function AdminTopBar(props) {
                 <div className="  flex ml-40nh-24 bg-forth-0 rounded-b-3xl  items-center justify-center">
                     <div className=" flex w-11/12 justify-between items-center">
                         <DashboardName name={props.name} roleName={props.roleName}/>
-                        <Profileicon loginuser={"User Names "} url="/imgs/admin/Admin One.jpg"/>
+                        <Profileicon loginuser={userName} url="/imgs/admin/Admin One.jpg"/>
                     </div>
                     
                 </div>
--- src/components/Atoms/serviceStation/ProfileIcon.js
+++ src/components/Atoms/serviceStation/ProfileIcon.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getCookie } from '../../../jsfunctions/cookies'
+import axios from 'axios';
 
 
 export default function ProfileIcon() {
+    const [userName, setuserName] = useState('User');
+    const userId = getCookie('userId');
+    const userType = getCookie('userType');
+    var config = {
+        headers: {
+            'Authorization': 'Bearer ' + getCookie('token'),
+        }
+    }
+    useEffect(() => {
+        axios.post(`${process.env.REACT_APP_API_BASE_URL}/auth/getUserName`, { "userId": userId, "userType": userType }, config)
+            .then(function (response) {
+                console.log(response.data);
+                
+                setuserName(response.data);
+            })
+    }, [])
     return (
         <div>
             <div className="m-5 flex items-center">
-                <img src="/imgs/hasa.png" className="w-16 h-16 rounded-full" />
-                <p className="text-white m-5 font-primary font-extralight">Hasantha Lakshan</p>
+                {/* <img src="/imgs/hasa.png" className="w-16 h-16 rounded-full" /> */}
+                <p className="text-white m-5 font-primary font-extralight">{userName}</p>
             </div>
         </div>
     )
