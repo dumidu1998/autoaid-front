5c0793fa01326a739174096089ac1854b418fad0
Dumidu Kasun Bandara Rajakaruna <50117273+dumidu1998@users.noreply.github.com>
2021-09-24 13:57:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt install -y docker.io
 docker volume create portainer_data
 docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
 docker run -e MYSQL_ROOT_PASSWORD=root -d --name mariadb --restart unless-stopped mariadb
-docker run --name myadmin -d --link 94bf68aa4237:db -p 8899:80 --restart unless-stopped phpmyadmin/phpmyadmin
+docker run --name myadmin -d --link mariadb:db -p 8899:80 --restart unless-stopped phpmyadmin/phpmyadmin
 docker run --name api -d --link mariadb -p 9009:9000  --restart unless-stopped dumidu1998/autoaid_api:latest
 docker run --name front  --restart unless-stopped -p 3000:3000 dumidu1998/autoaid_front
 
